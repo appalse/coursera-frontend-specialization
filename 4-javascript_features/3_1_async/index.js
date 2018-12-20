194ff153b22e8b29840e5c303b1e488a18d31f4f
appalse <kutina_nat@mail.ru>
2018-12-20 16:46:23 +0300

    use promises to solve task from week#3, javascript course

--- 4-javascript_features/3_1_async/index.js
+++ 4-javascript_features/3_1_async/index.js
@@ -2,6 +2,30 @@
  * @param {Function[]} operations
  * @param {Function} callback
  */
-module.exports = function (operations, callback) {
 
+module.exports = function (operations, callback) {
+	if (operations.length === 0) {
+		callback(null, []);
+		return;
+	}
+
+	let promises = operations.map(function(operation) {
+		return new Promise(function(resolve, reject) {
+			operation(function next(error, data) {
+				if (error === null) {
+					resolve(data);
+				} else {
+					reject(error);
+				}
+			});
+		});
+	});
+
+	Promise.all(promises)
+		.then(	function(results) {
+					callback(null, results);
+				}, 
+				function(error) {
+					callback(error, null);
+				});
 };
